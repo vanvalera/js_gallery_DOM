5ccfca36a7a516022f17a39aff7253856a0460a5
vanvalera <115824927+vanvalera@users.noreply.github.com>
2024-08-16 11:36:49 +0300

    make build

---
+++ index.7eb2cbea.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".gallery"),t=document.getElementById("largeImg");e.addEventListener("click",function(e){e.preventDefault();var r=e.target.closest(".list-item__link");t.src=r.href});
+//# sourceMappingURL=index.7eb2cbea.js.map
